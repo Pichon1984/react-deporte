6bbaf7a2e5db1c632f4e95fa64a7bae8bc92d240
pichon1984 <pichontecnico@gmail.com>
2025-12-21 20:59:32 -0300

    listo

--- src/PagesPrincipal/ForgotPasswordPage.jsx
+++ src/PagesPrincipal/ForgotPasswordPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import emailjs from "emailjs-com"; // 👈 importa EmailJS
+import emailjs from "@emailjs/browser";
 import "../styles/ForgotPasswordPage.css";
 
 const ForgotPasswordPage = () => {
